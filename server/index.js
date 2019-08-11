6f983bc45212af7e1ddf488681ca27854f34a916
Colin Richardson <richardsonjcolin@gmail.com>
2019-08-11 16:51:31 -0700

    remove pino from server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+// const pino = require('express-pino-logger')();
 const path = require('path');
 
 const app = express();
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 const apiRouter = require("./routes/api");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+// app.use(pino);
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/public')));
