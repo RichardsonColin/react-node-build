35161579c318582cb7cf16656dd0cdb165226062
Colin Richardson <richardsonjcolin@gmail.com>
2019-08-11 16:48:58 -0700

    serve up react front end through proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,7 +11,15 @@ const apiRouter = require("./routes/api");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/public')));
+
 // Routes
 app.use("/api", apiRouter);
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
